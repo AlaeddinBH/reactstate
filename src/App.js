08d55cbb07409efb3e1cbd0835e0601c2c9e760a
AlaeddinBH <alaeddinbenhammed@yahoo.fr>
2022-01-17 23:47:51 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
+import "./Style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    Person: {fullName:"Alaeddin", bio:"...........", imgSrc:'./img/photo.png', profession:'Automaticien', shows:true},
+    time : 0
+  }
+  
+  componentDidMount(){
+    setInterval(() => {
+      this.seconde()
+    }, 1000);
+  }
+
+  componentDidUpdate(){
+    
+  }
+
+  handelShow = () => this.setState({shows: !this.state.shows});
+  
+  seconde = () => this.setState({time: this.state.time + 1});
+
+  render() {
+    return (
+      <div className='App'>
+        <button className='btn' onClick={this.handelShow}>{this.state.shows ? "Hide" : "Show"}</button>
+        <h1 className='heading'>{this.state.time} s</h1>
+        {
+          this.state.shows&&
+        
+        <div className='box'>
+        <h1 className='heading'><span>Name:</span> {this.state.Person.fullName}</h1>
+        <h1 className='heading'><span> Bio:</span> {this.state.Person.bio}</h1>
+        <img  src={this.state.Person.imgSrc} alt=''/>
+        <h1 className='heading'><span> Profession:</span> {this.state.Person.profession}</h1>
+        </div>
+        }
+        
+      </div>
+    )
+  }
 }
 
-export default App;
